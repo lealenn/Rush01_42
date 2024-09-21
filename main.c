896cc82ac376a7df3c4d0b4b9d5c9130f18174d2
Lenny Chucherie <lchucher@z4r10p3.42lyon.fr>
2024-09-21 16:59:47 +0200

    added a start of ft_preshot fonction

--- main.c
+++ main.c
@@ -6,13 +6,38 @@
 /*   By: lchucher <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 14:40:25 by lchucher          #+#    #+#             */
-/*   Updated: 2024/09/21 15:07:00 by lchucher         ###   ########.fr       */
+/*   Updated: 2024/09/21 16:58:52 by lchucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+int	ft_atoi(char str);
+void	ft_preshot(char **tab, char *indic);
+
 int	main(int argc, char **argv)
 {
-	
+	int	indic[4];
+	int	tab[4][4];
+
+	indic[0] = ft_atoi(argv);
+	ft_preshot(tab, indic);
 }
+
+void	ft_preshot(char **tab, char *indic)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (indic[i] == 4)
+			/* don't know yet what to put there */
+		else if (indic[i] == 1)
+			/* same as above lol */
+		i++;
+	}
+}
+
+
